b22d51208f819a59a2f500bba61a018e4d9be6cc
Jack Neilson <jackdneilson@gmail.com>
2025-01-06 17:49:38 +0000

    First commit

---
+++ configuration.nix
@@ -0,0 +1,110 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Add support for virtualisation
+  virtualisation.vmware.guest.enable = true;
+
+  # Networking
+  networking.hostName = "lamb"; 
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+
+  # Localisation
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+  console.keyMap = "uk";
+
+  # Enable the X11 windowing system and i3
+  services.xserver = {
+    enable = true;
+    desktopManager.xterm.enable = false;
+    windowManager.i3.enable = true;
+  };
+  services.displayManager.defaultSession = "none+i3";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  # Enable sound with pipewire
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jack = {
+    isNormalUser = true;
+    description = "Jack Neilson";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    curl
+    git
+    ghostty
+    firefox
+    polybar
+  ];
+
+  # Programs with Options
+  # nvim
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  # Services
+  services.picom = {
+    enable = true;
+    shadow = true;
+    inactiveOpacity = 0.3;
+  };
+
+  # NixOS
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than +3";
+  };
+  nix.settings.auto-optimise-store = true;
+  system.stateVersion = "24.11";
+}
