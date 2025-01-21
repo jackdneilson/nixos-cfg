cfb8977d0abe6e7527c7baaa2ef54f49a19f8ba2
Jack Neilson <jackdneilson@gmail.com>
2025-01-21 16:34:16 +0000

    Moved bash config from home to yazi so it applies to all hosts

--- modules/user/yazi/default.nix
+++ modules/user/yazi/default.nix
@@ -1,6 +1,20 @@
 {
   programs.yazi = {
     enable = true;
-    enableBashIntegration = true;
+    # enableBashIntegration = true;
+  };
+
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+    '';
   };
 }
