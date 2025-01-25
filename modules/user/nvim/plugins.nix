77b2dbe3ab1caadcf04b35eec23e28176aac6a1c
Jack Neilson <jackdneilson@gmail.com>
2025-01-25 00:22:28 +0000

    Sorted autocomplete

--- modules/user/nvim/plugins.nix
+++ modules/user/nvim/plugins.nix
@@ -3,6 +3,8 @@
     enable = true;
     servers = {
       nixd.enable = true;
+      html.enable = true;
+      emmet_ls.enable = true;
     };
   };
 
@@ -14,6 +16,30 @@
   nvim-autopairs.enable = true;
   fugitive.enable = true;
   which-key.enable = true;
+  emmet.enable = true;
+
+  # Autocompletion
+  cmp = {
+    enable = true;
+    autoEnableSources = true;
+    settings = {
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "buffer"; }
+        { name = "luasnip"; }
+      ];
+      mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-e>" = "cmp.mapping.close()";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      };
+    };
+  };
 
   telescope = {
     enable = true;
